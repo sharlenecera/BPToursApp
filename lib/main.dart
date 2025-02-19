12509bfaf5b00d9315ba19549fa61d99e0035016
Sharlene Cera <cera.sharlene@gmail.com>
2025-02-19 22:43:35 +0000

    Started making the theme for the app's text and colours and imported google fonts' inter font.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final mainColour = const Color(0xFF326335);
+final backgroundColour = const Color(0xFFACD4AE);
+
+// Setting the theme for colours and text
+final ThemeData appTheme = ThemeData(
+  // primarySwatch: Colors.blue,
+  // primaryColor: Colors.blue,
+  // colorScheme: appTheme.colorScheme.copyWith(secondary: Colors.orange),
+  colorScheme:  ColorScheme.fromSeed(seedColor: mainColour),
+  scaffoldBackgroundColor: backgroundColour,
+  textTheme: GoogleFonts.interTextTheme(),
+  // textTheme: TextTheme(
+  //   headlineLarge: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.black),
+  //   bodyLarge: TextStyle(fontSize: 16.0, color: Colors.black),
+  // ),
+  buttonTheme: ButtonThemeData(
+    buttonColor: mainColour,
+    textTheme: ButtonTextTheme.primary,
+  ),
+  appBarTheme: AppBarTheme(
+    color: mainColour,
+    toolbarTextStyle: GoogleFonts.inter(
+      textStyle: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white),
+    ),
+    titleTextStyle: GoogleFonts.inter(
+      textStyle: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white),
+    ),
+  ),
+    useMaterial3: true,
+);
 
 void main() {
   runApp(const MyApp());
@@ -12,26 +44,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Login App',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        primarySwatch: Colors.blue,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: appTheme,
       home: LoginScreen(),
     );
   }
