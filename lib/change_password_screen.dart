8f8dde4cd7bfa450b101a7abbc5c01e286a46b64
Sharlene Cera <cera.sharlene@gmail.com>
2025-03-04 07:22:43 +0000

    Change Password page: Made the frontend.

--- lib/change_password_screen.dart
+++ lib/change_password_screen.dart
@@ -12,8 +12,105 @@ class ChangePasswordPage extends StatefulWidget {
 }
 
 class _ChangePasswordPageState extends State<ChangePasswordPage> {
+  String _errorMessage = '';
+  String _oldPassword = '';
+  String _newPassword = '';
+  String _repeatNewPassword = '';
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFFACD4AE),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFACD4AE),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text('Change Password', style: Theme.of(context).textTheme.displayMedium),
+          ],
+        ),
+        automaticallyImplyLeading: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text('Old Password', style: Theme.of(context).textTheme.headlineSmall,),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (value) => _oldPassword = value,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text('New Password', style: Theme.of(context).textTheme.headlineSmall,),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (value) => _newPassword = value,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text('Confirm New Password', style: Theme.of(context).textTheme.headlineSmall,),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (value) => _repeatNewPassword = value,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+            ),
+            if (_errorMessage.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Text(
+                  _errorMessage,
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                PrimaryButton(label: 'Submit', onPressed: (){}),
+                SecondaryButton(label: 'Cancel', onPressed: () => Navigator.pop(context)),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
